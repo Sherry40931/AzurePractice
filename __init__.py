29738e426e0fb7e29c79a2fe7b41ef6125ebae70
SherryChou <sherry18030@gmail.com>
2019-07-31 23:01:59 +0800

    add __init.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+
+def main(req):
+	import random
+	import numpy as np
+
+	# Generate 1000 ramdom number between 1 and 100
+	num = 1000
+	randomNumbers = [0]*num
+	for i in range(num):
+		randomNumbers[i] = random.randint(1,101)
+
+	# Compute std and var
+	std = np.std(randomNumbers)
+	var = np.var(randomNumbers)
+	print('std: %f, var; %f'% (std, var))
